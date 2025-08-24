d0e75b42c241f555cff194d080ffd4e53b71caea
Gani25 <memonabdulgani3d@gmail.com>
2025-08-24 12:42:15 +0530

    Form Changes

--- ecommerce/src/main/java/com/sprk/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/sprk/ecommerce/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.sprk.ecommerce.controller;
 
+import com.sprk.ecommerce.dto.ProductRequest;
 import com.sprk.ecommerce.entity.Image;
 import com.sprk.ecommerce.entity.Product;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,29 +20,14 @@ public class ProductController {
 
     @GetMapping("/admin/product")
     public String showProductForm(Model model){
-        Product product = new Product();
-        model.addAttribute("product", product);
+        ProductRequest productRequest = new ProductRequest();
+        model.addAttribute("productRequest", productRequest);
         return "productform";
     }
 
     @PostMapping("/admin/product")
-    public String processProductForm(@ModelAttribute("product") Product product, @RequestParam String imageUrls){
+    public String processProductForm(@Valid @ModelAttribute("productRequest") ProductRequest productRequest, BindingResult bindingResult){
 
-        System.out.println("All Images = "+imageUrls);
-        String[] imagePaths = imageUrls.split(", ");
-        List<Image> imageList = product.getProductImages();
-        if(imageList == null ){
-            imageList = new ArrayList<>();
-        }
-
-        for(String imagePath: imagePaths){
-            Image imageObj = new Image();
-            imageObj.setImagePathUrl(imagePath);
-            imageList.add(imageObj);
-
-        }
-        product.setProductImages(imageList);
-        System.out.println(product);
 
         return "#";
 
